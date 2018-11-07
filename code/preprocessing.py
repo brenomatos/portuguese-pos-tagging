89723074dcfb46dac85978de952026c22194b272
Breno <breno@dcc.ufmg.br>
2018-11-07 20:10:17 -0200

    code now creates different files with words and classes in the same order

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -3,13 +3,20 @@ import numpy as np
 
 # classes_list = []
 classes_set = set() # this list will keep track of all classes of words
-with open("../data/macmorpho-train.txt") as f:
+classes = open("words.txt","w")
+words = open("classes.txt", "w")
+with open("../data/macmorpho-train.txt","r") as f:
     text = f.read()
     words_list = text.split( )
     for word in words_list: # get all different tokens in text
         class_aux = word.split("_")
         classes_set.add(class_aux[1])
+        classes.write(str(class_aux[1])+" ")
+        words.write(str(class_aux[0])+" ")
 
+
+classes.close()
+words.close()
 
 # dataframe dimensions
 columns = ['classes']
@@ -22,6 +29,6 @@ one_hot_encoding = pd.get_dummies(df)
 one_hot_encoding.to_csv("one_hot.csv", index=False)
 
 # return every one_hot_encoding as numpy array
-for column in one_hot_encoding:
-    aux = np.array(one_hot_encoding[column])
-    print(column, aux)
+# for column in one_hot_encoding:
+#     aux = np.array(one_hot_encoding[column])
+#     print(column, aux)
